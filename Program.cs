bc4394ccf0453ff191ce480273aba0f410128369
Tharraketta <andreas.tharraketta@training.lug-ag.de>
2024-11-27 16:25:05 +0100

    second commit

--- Program.cs
+++ Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Aufgabe:
+            //Erstellen Sie einen Algorithmus der das Spiel FizzBuzz bis zur 100 durchführt.
+            //Regeln siehe https://de.wikipedia.org/wiki/Fizz_buzz.
+
+            //Modulo gibt uns den Rest einer Division als Ganzzahl zurück. Das Zeichen für den Operanten ist %
+            //Beispiel: 4 % 2 = 0
+
+            
+            Console.WriteLine("Willkommen bei FizzBuzz!");
+            for (int i = 1; i <= 100; i++) 
+            {
+                Thread.Sleep(1000);
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine($"{i}");
+                }
+            }
+            Console.WriteLine("Spiel beendet!");
+
+
         }
     }
 }
